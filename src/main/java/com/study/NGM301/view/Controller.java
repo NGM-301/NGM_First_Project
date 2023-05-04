af61a14cd489e9f1a6ff507754e3723eba5526e9
sgjang11 <sgjang11@outlook.kr>
2023-05-05 00:11:07 +0900

    test: index controller

--- src/main/java/com/study/NGM301/view/Controller.java
+++ src/main/java/com/study/NGM301/view/Controller.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-    @RequestMapping("/index")
+    @RequestMapping("/")
     public String test() {
-        return "index";
+        return "index.html";
     }
 
 }
